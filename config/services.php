bdad30553b16bc62e7329c1eaabf8108e718e9a8
timur <akimbaev.tim2002@gmail.com>
2023-12-12 16:53:44 +0600

    change url in config/services.php

--- config/services.php
+++ config/services.php
@@ -34,12 +34,12 @@ return [
     'google' => [
         'client_id' => '335516445980-mjidvqrvtd152k6nioc9pohr2ttrj2mk.apps.googleusercontent.com',
         'client_secret' => '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',
-        'redirect' => 'http://127.0.0.1:8000/login/google/callback',
+        'redirect' => 'http://batr.com.kz:8080/login/google/callback',
     ],
 
     'github' => [
         'client_id' => 'dcf9394d9179cd25c3bf',
         'client_secret' => 'c4ee752ebd3133eb8ac87f0f2dabfe942ee33a75',
-        'redirect' => 'http://127.0.0.1:8000/login/github/callback',
+        'redirect' => 'http://batr.com.kz:8080/login/github/callback',
     ],
 ];
